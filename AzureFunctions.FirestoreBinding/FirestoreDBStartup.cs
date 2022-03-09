c8ae280170cb65c83befb658baeafafff1e96f5c
Varun Teja <35555010+VarunSaiTeja@users.noreply.github.com>
2022-03-09 23:13:06 +0530

    config validation

--- AzureFunctions.FirestoreBinding/FirestoreDBStartup.cs
+++ AzureFunctions.FirestoreBinding/FirestoreDBStartup.cs
@@ -1,6 +1,10 @@
 ﻿using AzureFunctions.FirestoreBinding;
+using Google.Cloud.Firestore;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using System.Text;
 
 [assembly: WebJobsStartup(typeof(FirestoreDBStartup))]
 namespace AzureFunctions.FirestoreBinding
@@ -10,6 +14,30 @@ namespace AzureFunctions.FirestoreBinding
         public void Configure(IWebJobsBuilder builder)
         {
             builder.AddExtension<FirestoreConfigProvider>();
+            var firebaseSecret = Environment.GetEnvironmentVariable("FirebaseSecret");
+            if (!string.IsNullOrEmpty(firebaseSecret))
+            {
+                try
+                {
+                    firebaseSecret = Encoding.UTF8.GetString(Convert.FromBase64String(firebaseSecret));
+                    var projectId = JObject.Parse(firebaseSecret).Property("project_id").Value.ToString();
+
+                    var firestoreDb = new FirestoreDbBuilder
+                    {
+                        ProjectId = projectId,
+                        JsonCredentials = firebaseSecret
+                    }.Build();
+                    builder.Services.AddSingleton(firestoreDb);
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Problem with configuring firestore, please check your firebase secret format.");
+                    Console.WriteLine($"Refer this link for config steps: https://github.com/VarunSaiTeja/AzureFunctions.FirestoreBinding");
+                    Console.ResetColor();
+                }
+            }
         }
     }
 }
